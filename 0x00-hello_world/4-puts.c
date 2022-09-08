3f5bc1a50455cfcced6eac536b9832114c0936cc
chinaluccie <lucianne451@students.uonbi.ac.ke>
2022-09-08 15:47:18 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*printing a string using puts*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
